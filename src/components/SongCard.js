6b37ab3340566c4a36b91f13563d97a24041e59c
Ryan See <ryanjosephsee@gmail.com>
2022-12-06 11:13:53 -0800

    css started

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -15,22 +15,3 @@ function SongCard({title, artist, image}) {
   export default SongCard;
   
 
-
-// function SongCard({}) {
-
-    
-//   return (
-//     <li className="card">
-//       <img src={"https://via.placeholder.com/400"} alt={"song name"} />
-//       <h4>{"song name"}</h4>
-//       <p>Artist: {"artist name"}</p>
-//       {true ? (
-//         <button className="primary">View</button>
-//       ) : (
-//         <button>Favorite</button>
-//       )}
-//     </li>
-//   );
-// }
-
-// export default SongCard;
