d98ccfd56e3597718b3c755e1bc4fb451a4b068f
Ryan See <ryanjosephsee@gmail.com>
2022-12-07 14:37:49 -0800

    AddSongForm done

--- src/components/SongCard.js
+++ src/components/SongCard.js
@@ -12,9 +12,9 @@ function SongCard({title, artist, image, songURL}) {
     return (
       <li className="card">
         <img src={image} alt={title} />
-        <div className="ratio ratio-1x1">
+        {/* <div className="ratio ratio-1x1">
           <iframe width="200" height="100" src={songURL}></iframe>
-        </div>
+        </div> */}
         <h4>{title}</h4>
         <p>Artist: {artist}</p>
         <button  className='Play'>Play</button>
--- src/components/SongPage.js
+++ src/components/SongPage.js
@@ -1,6 +1,7 @@
 import React,{useState, useEffect} from "react";
 import SongList from "./SongList";
 import Search from "./Search";
+import AddSongForm from "./AddSongForm";
 const API="http://localhost:3000/songs"
 
 function SongPage() {
@@ -17,10 +18,15 @@ function SongPage() {
     return song.title.toLowerCase().includes(search.toLowerCase());
   })
 
+  function newSong(obj) {
+    setSongs([...songs, obj]);
+  }
+
   return (
     <main>
       <Search search ={search} setSearch={setSearch}/>
       <SongList songs={displayedSongs}/>
+      <AddSongForm newSong={newSong}/>
     </main>
   );
 }
