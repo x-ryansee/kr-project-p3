f868fb1b97eb2bde39e4a086e8286f730f29ce41
Keith Ma <keithchezzy@gmail.com>
2022-12-07 15:43:08 -0800

    morestyling

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,7 +4,7 @@ function About() {
         <div className="about">
             <h5>About Us</h5>
             <h6>Welcome to StudyParty</h6>
-            <p1>Welcome to the wonderful world of StudyParty streaming site. Where we curate the best of the best music made by music lovers and just for you! In StudyParty, we offer direct links to Study Music and party music for your mood and taste for your liking. So come along and discover your next hit song for your everyday needs.</p1>
+            <p1 className="about-p1">Welcome to the wonderful world of StudyParty streaming site. Where we curate the best of the best music made by music lovers and just for you! In StudyParty, we offer direct links to Study Music and party music for your mood and taste for your liking. So come along and discover your next hit song for your everyday needs.</p1>
             <p2>Created and Curated By Ryan See and Keith Ma</p2>
         </div>
     )
