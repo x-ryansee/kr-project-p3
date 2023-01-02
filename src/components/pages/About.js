49c9583f77412d05002d5a1030e735c99c87d3df
Keith Ma <keithchezzy@gmail.com>
2023-01-02 14:34:55 -0800

    new

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,9 +2,9 @@ function About() {
 
     return(
         <div className="about">
-            <h5>About Us</h5>
-            <h6>Welcome to Fusion Kitchen</h6>
-            <p1 className="about-p1">Welcome to the wonderful world of StudyParty streaming site. Where we curate the best of the best music made by music lovers and just for you! In StudyParty, we offer direct links to Study Music and party music for your mood and taste for your liking. So come along and discover your next hit song for your everyday needs.</p1>
+            <h5 className>About Us</h5>
+            {/* <h6>Fusion Kitchen</h6> */}
+            <p1>Welcome to Fusion kitchen. Where we curate the best of the best meals/cuisine just for you! </p1>
             <p2>Created and Curated By Ryan See and Keith Ma</p2>
         </div>
     )
