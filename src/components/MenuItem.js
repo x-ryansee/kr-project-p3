6a93aeeb75cf134eccfc52f6bf8d3e5041f40986
Keith Ma <keithchezzy@gmail.com>
2023-01-03 14:40:15 -0800

    add

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
 function MenuItem({ title, image, price}) {
-
-
+ 
 
     const [quantity, setQuantity] = useState(false)
- 
-  
+//  not sure if this works
+  function handleAddToCart(e) {
+    e.preventDefault();
+    var qty = document.getElementById("quantity");
+    qty.value = qty.value + 1;
+    setQuantity(quantity + 1);
+  }
   
     return (
       <li className="card">
-        <img height="200px" src={image} alt={title} />
+        <img height="190px" src={image} alt={title} />
         <h4>{title}</h4>
         <p>Price: {price}</p>
             <div className="d-flex align-items-center flex-column"
@@ -23,6 +27,9 @@ function MenuItem({ title, image, price}) {
                 </div>
                 <button onClick={() => setQuantity(quantity + 1)}>+</button>
               </div>
+              <button name="Add to cart"
+              className="Button"
+              onClick={handleAddToCart}> Add to Cart </button>
             </div>
       </li>
     );
--- src/components/MenuPage.js
+++ src/components/MenuPage.js
@@ -1,8 +1,6 @@
 import React,{useState, useEffect} from "react";
 import MenuList from "./MenuList";
 import Search from "./Search";
-// import Box from '@mui/material/Box';
-// import Slider from '@mui/material/Slider';
 const API="http://localhost:3000/foods"
 
 function MenuPage() {
@@ -18,15 +16,11 @@ function MenuPage() {
     return food.title.toLowerCase().includes(search.toLowerCase());
   })
 
-  // function valuetext(value: number) {
-  //   return `${value}°C`;
-  // }
+
   
   return (
     
     <main>
-       {/* <Box sx={{ width: 300 }}></Box> */}
-       
       <Search search ={search} setSearch={setSearch}/>
       <MenuList foods={displayedFoods}/>
     </main>
