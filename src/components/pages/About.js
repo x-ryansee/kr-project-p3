55703cfd5ed8e39aa3e1ac59ced5ffdfae3aa761
Keith Ma <keithchezzy@gmail.com>
2023-01-02 17:14:31 -0800

    morestuff

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,10 +2,10 @@ function About() {
 
     return(
         <div className="about">
-            <h5 className>About Us</h5>
+            <h5 className="H5">About Us</h5>
             {/* <h6>Fusion Kitchen</h6> */}
             <p1>Welcome to Fusion kitchen. Where we curate the best of the best meals/cuisine just for you! </p1>
-            <p2>Created and Curated By Ryan See and Keith Ma</p2>
+            <p2>Created by the masterminds of Ryan See and Keith Ma</p2>
         </div>
     )
 }
