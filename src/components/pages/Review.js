c57783f3e5286f3496f37493818d0cfc4bbfbf57
Keith Ma <keithchezzy@gmail.com>
2023-01-04 16:24:36 -0800

    in progress

--- src/components/pages/Review.js
+++ src/components/pages/Review.js
@@ -4,48 +4,47 @@ import ReviewList from "./ReviewList"
 const API="http://localhost:9393/reviews"
 
 function Review() {
-    const [items,setItems]=useState ([])
-  
-    useEffect(() =>{
-      fetch(API)
-      .then(res=>res.json())
-      .then (data=>setItems(data))
-    },[])
+  const [items,setItems]=useState ([])
+
+  useEffect(() =>{
+    fetch(API)
+    .then(res=>res.json())
+    .then (data=>setItems(data))
+  },[])
 console.log(setItems)
 console.log(items)
 
-
     return (
       <main>
-        <h1 style={{color: "blue"}}>Reviews</h1>
-        <form className="Form">
-      <fieldset>
-         <label className="Name">
-           <p>Name</p>
-           <input name="name"placeholder="Your name.." />
-           <p>Description</p>
-           <input name="Description" placeholder="Description.."/>
-           <p>Rating</p>
-           <input name="Rating" placeholder="Rating.."/>
-         </label>
-       </fieldset>
-       <button type="submit">Submit</button>
-      </form>
-      <ReviewList items={items} />
-      <div>
-      {items ? (
-        <ul>
-          {items.map((item) => (
-            <li key={item.id}>{item.name}{item.description}{item.rating}</li>
-          ))}
-        </ul>
-      ) : (
-        'Loading...'
-      )}
-    </div>
-      </main>
-      
-    );
+      <h1 style={{color: "blue"}}>Reviews</h1>
+      <form className="Form">
+    <fieldset>
+       <label className="Name">
+         <p>Name</p>
+         <input name="name"placeholder="Your name.." />
+         <p>Description</p>
+         <input name="Description" placeholder="Description.."/>
+         <p>Rating</p>
+         <input name="Rating" placeholder="Rating.."/>
+       </label>
+     </fieldset>
+     <button type="submit">Submit</button>
+    </form>
+    <ReviewList items={items} />
+    <div>
+    {items ? (
+      <li>
+        {items.map((item) => (
+          <li key={item.id}>{item.name}{item.description}{item.rating}</li>
+        ))}
+      </li>
+    ) : (
+      'Loading...'
+    )}
+  </div>
+    </main>
+    
+  );
 }
   
   export default Review;
